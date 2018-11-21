d7a24de35a5f7c03bce24cdf758f50bf4963aeff
buruihua <buruihua>
2018-11-21 16:47:57 +0800

    '修改了两个配置文件'

--- springcloud-eureka-sample/springcloud-eureka-client-customer/src/main/java/org/spring/springcloud/web/CustomerController.java
+++ springcloud-eureka-sample/springcloud-eureka-client-customer/src/main/java/org/spring/springcloud/web/CustomerController.java
@@ -1,9 +1,12 @@
 package org.spring.springcloud.web;
 
+import javafx.scene.chart.ValueAxis;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +20,9 @@ public class CustomerController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
 
+    @Value("${userName}")
+    private String userName;
+
     @Autowired
     private RestTemplate restTemplate; // HTTP 访问操作类
 
@@ -27,4 +33,11 @@ public class CustomerController {
 
         return "Hello,Customer! msg from provider : <br/><br/> " + providerMsg;
     }
+
+    @RequestMapping(value = "/getUserLogin" , method = RequestMethod.GET)
+    public String getUserLogin(){
+     return userName;
+    }
+
+
 }
